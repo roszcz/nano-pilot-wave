0407ee528b19af35d7a1a50928127e4130a72e2b
Mircea Eliade <roszcz@gmail.com>
2016-02-02 13:22:26 +0100

    Example bounceplot

--- fullerenpaths.py
+++ fullerenpaths.py
@@ -11,8 +11,8 @@ def read_pos(filepath):
 
     return out
 
-if __name__ == '__main__':
-    
+def show_fullerene():
+    """ Show results for fullerene case OBSOLETE """
     apos = read_pos('data/a_fullerene_pos.dat')
 
     x = [pos[0] for pos in apos]
@@ -23,3 +23,20 @@ if __name__ == '__main__':
     plt.show()
     plt.plot(z)
     plt.show()
+
+def show_single_ball():
+    """ Better fixed hexagonal structure with heave bouncer """
+    apos = read_pos('data/single_ball.dat')
+
+    t           = [pos[0] for pos in apos]
+    z_ball      = [pos[1] for pos in apos]
+    z_membrane  = [pos[2] for pos in apos]
+
+    plt.plot(t, z_ball, c='black')
+    plt.plot(t, z_membrane, c='green')
+    plt.legend(['ball', 'membrane'], loc = 'center right')
+    plt.show()
+
+if __name__ == '__main__':
+    """ %run this.file.py """
+    show_single_ball()
