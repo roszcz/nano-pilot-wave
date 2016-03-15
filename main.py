b08ad8b21300f165d2dfc04a8e3ae481e75aca82
Mircea Eliade <roszcz@gmail.com>
2016-03-15 14:59:40 +0100

    Single slit research
    - no interference

--- main.py
+++ main.py
@@ -38,6 +38,8 @@ class LampsRunner(object):
         self.a_ball_x            = '10'
         self.a_ball_y_m          = 'a_ball_y'
         self.a_ball_y            = '10'
+        self.a_ball_y_vel_m      = 'a_ball_y_vel'
+        self.a_ball_y_vel        = '0.013'
 
     def set_membrane_bond_harmonic_constant(self, kz):
         """ wow """
@@ -91,6 +93,10 @@ class LampsRunner(object):
         """ STarting position x """
         self.a_ball_x = str(x)
 
+    def set_a_ball_y_vel(self, vel):
+        """ Starting velocity """
+        self.a_ball_y_vel = str(vel)
+
     def run_it(self, filepath):
 	""" Runs lammps """
         # Unix mp-ready version
@@ -108,6 +114,7 @@ class LampsRunner(object):
                     '-var', self.mb_bond_r0_marker, self.mb_bond_r0,
                     '-var', self.a_ball_x_m, self.a_ball_x,
                     '-var', self.a_ball_y_m, self.a_ball_y,
+                    '-var', self.a_ball_y_vel_m, self.a_ball_y_vel,
                     '-in', filepath]
 
         # Windows slow version
@@ -170,15 +177,19 @@ if __name__ == '__main__':
     runner.set_a_ball_mass(a_ball_mass)
 
     # Membrane size
-    sheet_radius    = 70
+    sheet_radius    = 88
     runner.set_sheet_radius(sheet_radius)
 
     # Ball starting postion-y
-    y_position = [10 + 5 * np.random.random() for _ in range(400)]
+    y_position = [50 + 5 * np.random.random() for _ in range(1000)]
 
     # And x
     x_position = 150
     runner.set_a_ball_x(x_position)
+
+    # Starting velocity
+    y_velocity = 0.01 + 19*0.001
+    runner.set_a_ball_y_vel(y_velocity)
 
     # Declare score paths
     ball_file = 'data/a_ball.dat'
@@ -189,7 +200,7 @@ if __name__ == '__main__':
     membranes_z = []
 
     # Final settings
-    runner.set_number_of_iterations(7000)
+    runner.set_number_of_iterations(6000)
     runner.set_number_of_cores(8)
 
     for val in y_position:
